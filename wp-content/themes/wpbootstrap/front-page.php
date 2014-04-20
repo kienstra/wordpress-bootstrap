702b64388eb5da9cde246a53c6d7a8e6ac585b3c
Ryan Kienstra <kienstraryan@gmail.com>
2014-04-20 11:27:45 -0700

    Front page copy

--- wp-content/themes/wpbootstrap/front-page.php
+++ wp-content/themes/wpbootstrap/front-page.php
@@ -14,7 +14,7 @@
 <div class="container">
 <h1>Hello, world!</h1>
 <p>
-This is a new template for a simple but extremely useful marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.
+This is a new template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.
 </p>
 <a class="btn btn-primary btn-lg" role="button">Learn</a>
 
