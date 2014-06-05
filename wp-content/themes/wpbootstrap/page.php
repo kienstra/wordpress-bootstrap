fab84e548562672298ba07e1c3fffcc094a1c27c
Ryan Kienstra <kienstraryan@gmail.com>
2014-06-04 23:43:52 -0700

    page.php: edited out byline and extra title

--- wp-content/themes/wpbootstrap/page.php
+++ wp-content/themes/wpbootstrap/page.php
@@ -1,26 +1,15 @@
 <?php get_header(); ?>
 
 <div class="row">
-  PAGE.PHP
   <div class="col-md-8">
-    <h1><?php wp_title('') ; ?></h1>
-  
+    <h1><?php wp_title('') ; ?></h1>  
   <?php if ( have_posts() ) :  while ( have_posts() ) : the_post() ; ?>
       <article class="post">
-        <h2><a href="<?php the_permalink() ; ?>"><?php the_title() ; ?></a></h2>
-      <p><em>
-        By: <?php the_author() ; ?>
-        on: <?php the_time( 'l, F jS, Y' ) ; ?>
-	in: <?php the_category( ', ' ) ; ?>,
-	<?php if ( have_comments() ) : ?>
-	<a href="<?php comments_link() ; ?>"><?php comments_number() ; ?></a>
-	<?php endif ; ?>
-      </em></p>
-      <?php the_excerpt() ; ?>
+	<?php the_content ; ?>
       <hr>
       </article>
   <?php  endwhile; else: ?>
-    <p><?php _e('Sorry, there are no posts' ); ?></p>
+    <p><?php _e('Sorry, there is no text' ); ?></p>
     <?php 
     endif; ?>
      <?php wp_reset_query() ; ?> 
