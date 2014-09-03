52017cfaa06ccabfed2991dd17af96b927b18528
Ryan Kienstra <kienstraryan@gmail.com>
2014-09-02 21:04:50 -0700

    style.css: .top-navbar

--- wp-content/themes/wpbootstrap/page.php
+++ wp-content/themes/wpbootstrap/page.php
@@ -3,7 +3,6 @@
 <?php get_header() ; ?>
   <div class="row">
     <div class="col-md-8">
-    PAGE.php
       <?php if ( have_posts() ) :  while ( have_posts() ) : the_post() ; ?>
         <?php get_template_part( 'content' , 'page' ) ; ?>
       <?php endwhile; else: 
