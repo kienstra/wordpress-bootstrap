c2ff0a5022e9a9b89cfec2e7bdec4c70f9f59bc6
Ryan Kienstra <kienstraryan@gmail.com>
2014-09-02 22:43:16 -0700

    git add page-full-width.php

--- wp-content/themes/wpbootstrap/home.php
+++ wp-content/themes/wpbootstrap/home.php
@@ -5,32 +5,13 @@
   <div class="col-md-8">
     <h1><?php wp_title('') ; ?></h1>
   <?php if ( have_posts() ) :  while ( have_posts() ) : the_post() ; ?>
-          <article <?php post_class( 'post-preview' ) ; ?>>
-	    <a href="<?php the_permalink() ; ?>">
-      	      <?php  the_post_thumbnail(
-	      '' , array( 'class' => 'pull-right img-responsive img-rounded home-wp-post-image home-featured-image' ) ) ;  ?>
-	    </a>
-	    <h2>
-	      <a href="<?php the_permalink() ; ?>"><?php the_title() ; ?></a>
-      	    </h2>
-	    <p>
-	        <?php bwp_author_date_category_tag() ; ?>
-		<a href=<?php the_permalink() ; ?>" class="mobile-post-permalink">
-		  <span class="glyphicon glyphicon-chevron-right">
-		</a>
-	    </p>
-	    <?php the_excerpt() ; ?>
-
-         <hr>
-        </article>	  
-   
+    <?php get_template_part( 'content' , 'post-preview' ) ; ?>
   <?php  endwhile; else: ?>
     <p><?php _e('Sorry, there are no posts' ); ?></p>
     <?php 
     endif; ?>
      <?php wp_reset_query() ; ?> 
     <?php bwp_paginate_links() ; ?>
-  
   </div>
   <div class="col-md-4"> <!--span4 -->
     <?php if ( dynamic_sidebar( 'main_sidebar' ) ) ; ?>
